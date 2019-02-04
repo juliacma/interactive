ca6e03c8fd69529e7f24d5c23804b50cbca0334d
Julia <julia.ma@yale.edu>
2019-02-04 05:50:57 -0500

    Exercise2

---
+++ exercise2/writing_script.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+text = open("writing.txt", "r")
+
+wordList = []
+
+for line in text:
+	for word in line.split():
+		word=word.strip()
+		wordList.append(word)
+
+text.close()
+
+finalText = open("writing-final.txt", "w")
+
+for i in range(0, len(wordList)):
+	finalText.write('<a href="#'+str(i+2)+'"><div id="'+str(i+1)+'">'+wordList[i]+'</div></a>\n')
+
+finalText.close()
